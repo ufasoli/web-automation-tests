f390cc73eb77f4d54e6f99fc8b8ca93e1a41bdb6
Ulises Fasoli <ufasoli@gmail.com>
2013-09-26 10:08:33 +0200

    extracted the text values of the tests to an external file

---
+++ simple-webapp/src/test/java/util/ConstantsLoader.java
@@ -0,0 +1,14 @@
+package util;
+/**
+ * Created with IntelliJ IDEA.
+ * User: ULF
+ * Date: 26.09.13
+ * Time: 09:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConstantsLoader {
+
+    static {
+
+    }
+}
