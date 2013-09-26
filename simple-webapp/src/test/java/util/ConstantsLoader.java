5113cb39f78d40f03d8bc47e0d5b6542a672a57c
Ulises Fasoli <ufasoli@gmail.com>
2013-09-26 10:09:50 +0200

    constants properties loading logic

--- simple-webapp/src/test/java/util/ConstantsLoader.java
+++ simple-webapp/src/test/java/util/ConstantsLoader.java
@@ -1,14 +1,54 @@
 package util;
+
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Created with IntelliJ IDEA.
- * User: ULF
+ * User: Ulises Fasoli
  * Date: 26.09.13
  * Time: 09:22
- * To change this template use File | Settings | File Templates.
  */
 public class ConstantsLoader {
 
-    static {
+    private static ConstantsLoader instance = null;
+    private Properties constants;
 
+
+    private ConstantsLoader() {
+        final String propertiesFileName = "values.properties";
+
+        constants = new Properties();
+
+        InputStream inputStream = ConstantsLoader.class.getClassLoader().getResourceAsStream(propertiesFileName);
+
+
+        if (inputStream == null) {
+
+            throw new RuntimeException(String.format("Property file %s. cannot be found in the classpath", propertiesFileName));
+
+        }
+
+
+        try {
+            constants.load(inputStream);
+        } catch (Exception e) {
+            throw new RuntimeException(String.format("An error occurred while loading the properties file : %s. Error : %s", propertiesFileName, e.getMessage()));
+        }
+
+
+    }
+
+    public static ConstantsLoader loader() {
+
+        if (instance == null) {
+            instance = new ConstantsLoader();
+        }
+
+        return instance;
+    }
+
+    public Properties getConstants() {
+        return constants;
     }
 }
