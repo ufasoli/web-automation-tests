142d5a98a87b824777bc0e521ef640cb7028d58d
Ulises Fasoli <ufasoli@gmail.com>
2013-10-15 12:50:49 +0200

    Added properties wrappers and loaders as well as a configuration file

---
+++ simple-webapp/src/test/java/util/ConfigLoader.java
@@ -0,0 +1,54 @@
+package util;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ulises Fasoli
+ * Date: 26.09.13
+ * Time: 09:22
+ */
+public class ConfigLoader {
+
+    private static ConfigLoader instance = null;
+    private Properties constants;
+
+
+    private ConfigLoader() {
+        final String propertiesFileName = "values.properties";
+
+        constants = new Properties();
+
+        InputStream inputStream = ConfigLoader.class.getClassLoader().getResourceAsStream(propertiesFileName);
+
+
+        if (inputStream == null) {
+
+            throw new RuntimeException(String.format("Property file %s. cannot be found in the classpath", propertiesFileName));
+
+        }
+
+
+        try {
+            constants.load(inputStream);
+        } catch (Exception e) {
+            throw new RuntimeException(String.format("An error occurred while loading the properties file : %s. Error : %s", propertiesFileName, e.getMessage()));
+        }
+
+
+    }
+
+    public static ConfigLoader loader() {
+
+        if (instance == null) {
+            instance = new ConfigLoader();
+        }
+
+        return instance;
+    }
+
+    public Properties getConstants() {
+        return constants;
+    }
+}
---
+++ simple-webapp/src/test/java/util/PropertiesLoader.java
@@ -0,0 +1,10 @@
+package util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ulises Fasoli
+ * Date: 15.10.13
+ * Time: 09:56
+ */
+public interface PropertiesLoader {
+}
