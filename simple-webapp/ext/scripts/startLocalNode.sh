3026f725a954391599870a75c6099b039264da65
Ulises Fasoli <ufasoli@gmail.com>
2013-10-18 10:53:47 +0200

    updated readme and pom

--- simple-webapp/ext/scripts/startLocalNode.sh
+++ simple-webapp/ext/scripts/startLocalNode.sh
@@ -1,1 +1,2 @@
-java -jar selenium-server-standalone-2.14.0.jar -role hub
+#!/bin/bash
+java -jar C:\dev\bin\selenium-server\selenium-server-standalone-2.36.0.jar -role node  -hub http://localhost:4444/grid/register -Dwebdriver.chrome.driver=C:\dev\workspaces\idea\web-automation-tests\simple-webapp\ext\webdrivers\chrome\chromedriver_windows.exe
